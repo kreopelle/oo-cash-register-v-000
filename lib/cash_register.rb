66148f4050d095649a07b48332146c62155a396d
Kayla Reopelle <kreopelle@gmail.com>
2018-05-20 14:42:21 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -34,7 +34,7 @@ class CashRegister
   end
 
   def apply_discount
-      if @discount == nil
+      if @discount == 0 || @discount == nil
         "There is no discount to apply."
       else
         @total = @total - (@total * (@discount/100.0))
