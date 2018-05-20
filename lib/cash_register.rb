45f0d5456b3a17c8a13998e54b7c368e3c846712
Kayla Reopelle <kreopelle@gmail.com>
2018-05-20 13:45:55 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,40 @@
+#create an object oriented cash Register
+# add items with different quanities, prices
+# calculate discounts
+# keep track of what's been added
+# void last transaction
+# to call an instance method inside of another instance method, use self keyword
 
+class CashRegister
+  attr_accessor :total, :employee_discount
+
+  def initialize(employee_discount = 20)
+    @employee_discount = employee_discount
+    @total = 0
+  end
+
+  def discount
+    @total = @total - (@total * @employee_discount)
+  end
+
+  def total
+
+  end
+
+  def add_item
+
+  end
+
+  def apply_discount
+
+  end
+
+  def items
+
+  end
+
+  def void_last_transaction
+
+  end
+
+end
